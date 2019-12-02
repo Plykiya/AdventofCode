5180e017dafedbba9ea792e6fd6661cb2bc37aa0
Plykiya <58439124+Plykiya@users.noreply.github.com>
2019-12-02 14:49:24 -0500

    Update day2.php

--- day2.php
+++ day2.php
@@ -1,11 +1,9 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 class manipulation {
   /* Position will maintain index as we skip 4 in the array between functions */
   public $finalvalue;
+  public $noun;
+  public $verb;
   public $position;
   public $inputarray;
   
@@ -14,9 +12,6 @@ class manipulation {
     $this->inputarray = $input;
     $this->position = 0;
     
-    /* Extra instructions given in the problem */
-    $this->inputarray[1] = 12;
-    $this->inputarray[2] = 2;
   }
   
   public function interpretation() {
@@ -61,6 +56,7 @@ class manipulation {
     return $sum;
   }
   
+  /* The multiplication version of function one */
   private function two() {
     $position1 = $this->inputarray[$this->position+1];
     $position2 = $this->inputarray[$this->position+2];
@@ -70,19 +66,52 @@ class manipulation {
     return $product;
   }
   
+  public function nounverb($noun, $verb) {
+    $this->inputarray[1] = $noun;
+    $this->inputarray[2] = $verb;
+  }
+  
+  public function resetdata($array) {
+    $this->inputarray = $array;
+    $this->position = 0;
+  }
 }
 
+
+
+/* Input data and creating an array */
 $input = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,19,1,13,19,23,2,23,9,27,1,6,27,31,2,10,31,35,1,6,35,39,2,9,39,43,1,5,43,47,2,47,13,51,2,51,10,55,1,55,5,59,1,59,9,63,1,63,9,67,2,6,67,71,1,5,71,75,1,75,6,79,1,6,79,83,1,83,9,87,2,87,10,91,2,91,10,95,1,95,5,99,1,99,13,103,2,103,9,107,1,6,107,111,1,111,5,115,1,115,2,119,1,5,119,0,99,2,0,14,0";
 $array = explode(",", $input);
 $arraything = new manipulation($array);
 
 /* Initialize $i for the loop, will end once the position reaches an element of 99 */
 $i = 0;
+$arraything->nounverb(12, 2);
 
 /* Will constantly loop over the function that determines which function to use and advances the position */
 while ($i != 99) {
   $i = $arraything->interpretation();
 }
+echo "$arraything->finalvalue \n";
 
-echo "$arraything->finalvalue";
+
+
+for ($j = 0; 0 <= $j && $j <= 99; $j++){
+  for ($k = 0; 0 <= $k && $k <= 99; $k++) {
+    /* Resetting loop condition and data after completion */
+    $arraything->resetdata($array);
+    $arraything->nounverb($j, $k);
+    $i = 0;
+    
+    /* Same loop as before, but checks the final value each time and then breaks out of all outside loops once successful */
+    while ($i != 99) {
+      $i = $arraything->interpretation();
+        if ($arraything->finalvalue == 19690720) {
+          $nounverbthing = 100 * $arraything->inputarray[1] + $arraything->inputarray[2];
+          echo "$nounverbthing \n";
+          break 3;
+      }
+    }
+  }
+}
 ?>
